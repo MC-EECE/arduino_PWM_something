7b9ea2438bc6fc5e4c0858cbfee91290412dab6d
N3W2GitHub <31664279+N3W2GitHub@users.noreply.github.com>
2017-10-31 13:48:12 -0400

    Add files via upload

---
+++ arduino_Potentiometer_PWM.ino
@@ -0,0 +1,12 @@
+int pot_value = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(A0, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+pot_value = analogRead(A0)>>2;
+analogWrite(LED_BUILTIN, pot_value);
+}
