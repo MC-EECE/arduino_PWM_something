1d747cdccc71dcf0d12b8d3665941cccbdf47f60
N3W2GitHub <31664279+N3W2GitHub@users.noreply.github.com>
2017-11-09 12:30:02 -0500

    Update arduino_Potentiometer_PWM.ino

--- arduino_Potentiometer_PWM.ino
+++ arduino_Potentiometer_PWM.ino
@@ -8,7 +8,7 @@ pinMode(10, OUTPUT);
 
 void loop() {
   // put your main code here, to run repeatedly:
-pot_value = analogRead(A0)>>2;        //Writing the read-in pot value to the pot_value variable
+pot_value = analogRead(A0)>>2;        //Writing the read-in pot value to the pot_value variable and scaling to 8 bits (divide by 4)
 analogWrite(LED_BUILTIN, pot_value);  //Writing pot_value to LED
 analogWrite(7, pot_value);            //Writing pot_value to pin 7
 }
